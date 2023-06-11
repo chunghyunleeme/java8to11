63ddba0d9f63e41b186f60ec518a6e38e4f1e36b
chunghyunleeme <chunghyun.dev@gmail.com>
2023-06-12 00:58:31 +0900

    자바에서 제공하는 함수형 인터페이스

--- src/main/java/dev/chunghyun/Main.java
+++ src/main/java/dev/chunghyun/Main.java
@@ -1,9 +1,39 @@
 package dev.chunghyun;
 
+import java.util.function.*;
+
 public class Main {
     public static void main(String[] args) {
+        int baseNumber = 10;
         // 익명 내부 클래스
-        RunSomething runSomething = () -> System.out.println("Hello!");
-        runSomething.doIt();
+        RunSomething runSomething = number -> number + baseNumber;
+        System.out.println(runSomething.doIt(1));
+
+        Plus10 plus10 = new Plus10();
+        System.out.println(plus10.apply(2));
+
+        Function<Integer, Integer> function = number ->  number + 10;
+        System.out.println(function.apply(3 ));
+
+        Function<Integer, Integer> multiply2 = number -> number * 2;
+        System.out.println(multiply2.apply(7));
+
+        Function<Integer, Integer> minus1 = number -> number - 1;
+        System.out.println(plus10.compose(multiply2).andThen(minus1).apply(3));
+
+        Consumer<Integer> printT = i -> System.out.println(i);
+        printT.accept(16);
+
+        Supplier<Integer> get17 = () -> 17;
+        System.out.println(get17.get());
+
+        Predicate<Integer> isEven = number -> (number & 1) == 0;
+        System.out.println(isEven.test(18));
+
+        Predicate<String> startsWithChunghyun = s -> s.startsWith("chunghyun");
+        System.out.println(startsWithChunghyun.test("ChunghyunLee"));
+
+        UnaryOperator<Integer> unaryOperator = number -> number + 10;
+        System.out.println(unaryOperator.apply(8));
     }
 }
--- src/main/java/dev/chunghyun/RunSomething.java
+++ src/main/java/dev/chunghyun/RunSomething.java
@@ -2,5 +2,5 @@ package dev.chunghyun;
 
 @FunctionalInterface// 추상 메서드가 1개만 있으면 함수형 인터페이스
 public interface RunSomething {
-    void doIt();
+    int doIt(int number);
 }
